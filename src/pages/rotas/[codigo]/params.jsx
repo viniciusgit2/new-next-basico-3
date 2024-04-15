62229ef6a887d4dc26f07d814e313142bca5f259
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-15 10:05:34 -0300

    aparecendo na tela as rotas

--- src/pages/rotas/[codigo]/params.jsx
+++ src/pages/rotas/[codigo]/params.jsx
@@ -3,10 +3,11 @@ import { useRouter } from "next/router"
 export default function params(){
     const router =useRouter()
   
- 
+ const id=router.query.id
+ const nome =router.query.nome
 
     console.log(router)
-    return('rotas')
+    return([id,nome])
 
     
 }
