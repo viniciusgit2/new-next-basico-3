a714606f5d48a3cd5db4ab02043eb4bb26efb3fa
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-16 08:32:19 -0300

    questão versao resumida

---
+++ src/pages/api/[id].js
@@ -0,0 +1,12 @@
+export default function questao(req,res){
+    if (req.method ==='GET'){
+        const id=req.query.id
+        res.status(200).json({
+            id,
+            enuncido:"qual é sua cor favorita",respotas:['branca','vermelha','verde']})
+        
+    }
+    else{
+        res.status(405).send
+}
+}
---
+++ src/pages/api/metodo.js
@@ -0,0 +1,10 @@
+export default(req,res)=>{
+
+    if (req.method==='GET'){
+    res.status(200).json({nome:"vini"})
+}
+else{
+    res.status(200).json({nome:"vini.patel.fin"})
+
+}
+}
